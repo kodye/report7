db6bc3235224c57b4d5072b7c430d9687a09cd3b
kodai kina <e165724@ie.u-ryukyu.ac.jp>
2017-02-05 14:17:03 +0900

    report7

---
+++ src/main/java/jp/ac/uryukyu/ie/e165724/Main.java
@@ -0,0 +1,19 @@
+package jp.ac.uryukyu.ie.e165724;
+
+import javax.swing.JFrame;
+
+public class Main{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        MainFrame panel = new MainFrame();
+        frame.add(panel);
+
+        frame.setTitle("ƒuƒƒbƒN•ö‚µ");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+//		frame.setBounds(100, 100, MainFrame.WIDTH+15, MainFrame.HEIGHT+50);
+        frame.setBounds(200, 200, MainFrame.WIDTH+15, MainFrame.HEIGHT+39);
+        panel.start();
+    }
+}
